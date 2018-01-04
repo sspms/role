536442ffcd32c803b2ec93604f209597fd88738b
I336253 <david.du@sap.com>
2018-01-04 15:48:47 +0800

    add eureka discover add configure entry in config file for spring.cloud.client

--- src/main/groovy/com/shanshui/smartcommunity/role/RoleApplication.groovy
+++ src/main/groovy/com/shanshui/smartcommunity/role/RoleApplication.groovy
@@ -2,10 +2,12 @@ package com.shanshui.smartcommunity.role
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = ['com.shanshui.smartcommunity.clients'])
+@EnableDiscoveryClient
 class RoleApplication {
 
     static void main(String[] args) {
